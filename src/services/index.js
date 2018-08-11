b3070c9a619a0fa0a1ab2e6d8cf15664fd3a646b
Ezekiel Mangalu <emangalu@gmail.com>
2018-08-11 15:43:11 +0800

    Added accounts service

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const transactions = require('./transactions/transactions.service.js');
+const accounts = require('./accounts/accounts.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(transactions);
+  app.configure(accounts);
 };
