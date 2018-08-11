7df86d9f532bd8b57dd6354fdd434bf85a0e550a
Ezekiel Mangalu <emangalu@gmail.com>
2018-08-11 16:53:18 +0800

    Setup routing and refactored components

--- src/client.js
+++ src/client.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './views/App'
+import Dashboard from './views/Dashboard'
+import TransactionList from './views/components/TransactionList'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Dashboard },
+  { path: '/all', component: TransactionList }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
-  el: "#app",
+  el: '#app',
+  router,
   render: h => h(App)
 })
---
+++ src/views/components/TransactionList.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>All transactions</h1>
+</template>
+
+<style lang="scss" scoped>
+  @import '../../style/helpers';
+
+  h1 {
+    @extend %heading;
+    padding: var(--space);
+  }
+</style>
