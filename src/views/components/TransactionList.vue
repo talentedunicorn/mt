89a2bf00495e886bd21cd85b66f3df53d5363544
Ezekiel Mangalu <emangalu@jirnexu.com>
2018-11-26 14:11:16 +0800

    Added content to transaction list

--- src/views/components/TransactionList.vue
+++ src/views/components/TransactionList.vue
@@ -1,10 +1,28 @@
 <template>
-  <h1>All transactions</h1>
+  <div>
+    <h1>All transactions</h1>
+    <Transaction v-for="(item, i) in transactions" :key="i" :currency="currency" :data="item"/>
+  </div>
 </template>
 
 <script>
+  import Transaction from '../components/Transaction'
   export default ({
-    name: "Transaction list"
+    name: "TransactionList",
+    components: {
+      Transaction
+    },
+    computed: {
+      transactions () {
+        return this.$store.state.transactions
+      },
+      currency () {
+        return this.$store.state.currency
+      }
+    },
+    mounted() {
+      this.$store.dispatch('fetchData')
+    }
   })
 </script>
 
